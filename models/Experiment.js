623d2b22b505e7ac3ab3b18717438d6818fff303
SanaMushtaq <13besesmushtaq@seecs.edu.pk>
2020-04-09 17:16:42 +0100

    Create Experiment.js

---
+++ models/Experiment.js
@@ -0,0 +1,32 @@
+const mongoose = require('mongoose');
+
+const ExperimentSchema = new mongoose.Schema({
+    id: {
+        type: String,
+        required: true
+    },
+    title: {
+        type: String,
+        required: true
+    },
+    description: {
+        type: String,
+        required: true
+    },
+    tasks: { 
+        type: Array,
+        required: true
+    },
+    status: {
+        type: String,
+        required: true
+    },
+    date: {
+        type: Date,
+        default: Date.now
+    },
+});
+
+const Experiment = mongoose.model('Experiment', ExperimentSchema);
+
+module.exports = Experiment;
