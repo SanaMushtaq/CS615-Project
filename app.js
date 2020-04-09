c4d8a90cd9e1123be01339308093a846773563ea
SanaMushtaq <13besesmushtaq@seecs.edu.pk>
2020-04-09 17:14:01 +0100

    Add files via upload

---
+++ app.js
@@ -0,0 +1,59 @@
+const express = require('express');
+const expressLayouts = require('express-ejs-layouts');
+const mongoose = require('mongoose');
+const flash = require('connect-flash');
+const session = require('express-session')
+const passport = require('passport')
+
+const app = express();
+
+//PASSPORT Config
+require('./config/passport')(passport);
+//DB Config
+const db = require('./config/keys').MongoURI;
+
+//Connect to DB
+mongoose.connect(db,{ 
+    useNewUrlParser: true,
+    useUnifiedTopology: true })
+    .then(() => console.log('MongoDB Connected'))
+    .catch(err => console.log(err));
+
+//EJS
+app.use(expressLayouts);
+app.set('view engine', 'ejs');
+
+//BODYPARSER
+app.use(express.urlencoded({ extended: false }));
+
+//SESSION
+app.use(session({
+    secret: 'secret',
+    resave: true,
+    saveUninitialized: true,
+}));
+
+//PASSPORT MIDDLEWARE
+app.use(passport.initialize());
+app.use(passport.session());
+
+//FLASH
+app.use(flash());
+
+//Global variables
+app.use((req, res, next)=> {
+    res.locals.success_msg = req.flash('success_msg');
+    res.locals.error_msg = req.flash('error_msg');
+    res.locals.error = req.flash('error');
+    next();
+})
+
+
+//ROUTES
+app.use('/', require('./routes/index'));
+app.use('/users', require('./routes/users'));
+app.use('/experiments', require('./routes/experiments'));
+
+const PORT = process.env.PORT || 3000;
+
+app.listen(PORT, console.log(`Server started on PORT ${PORT}`));
