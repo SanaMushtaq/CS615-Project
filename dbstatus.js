3ed3064607d42d250f5a832bffc8e6a462526bbb
prcshen <60744832+prcshen@users.noreply.github.com>
2020-05-03 17:14:08 -0400

    Create dbstatus.js
    to change status on database when a exp closes

---
+++ dbstatus.js
@@ -0,0 +1,12 @@
+app.put('/changestatus',function(req,res){
+    let newpw=req.body['newstatus'];
+    console.log(req.session.name.status);
+    Experiment.updateOne({name:'exp4'},{status:"close"},function(err,raw){
+        if(raw.ok==1){
+            console.log('changestatus success');
+            res.send('success');
+        }else{
+            res.send('fail');
+        }
+    });
+});
